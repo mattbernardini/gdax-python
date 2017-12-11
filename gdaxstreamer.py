70018e38f2dd820fa77efe11d1563712e6c6e369
mattbernardini <matt.bernardini@gmail.com>
2017-12-10 19:20:24 -0800

    woot woot

---
+++ gdaxstreamer.py
@@ -0,0 +1,21 @@
+#!/bin/python
+# import PyMongo and connect to a local, running Mongo instance
+from pymongo import MongoClient
+import gdax
+mongo_client = MongoClient("mongodb://localhost:27017/")
+
+# specify the database and collection
+db = mongo_client.cryptocurrency_database
+BTC_collection = db.BTC_collection
+ETH_collection = db.ETH_collection
+LTC_collection = db.LTC_collection
+# instantiate a WebsocketClient instance, with a Mongo collection as a parameter
+wsClient1 = gdax.WebsocketClient(url="wss://ws-feed.gdax.com", products="BTC-USD",
+  mongo_collection=BTC_collection, should_print=True)
+wsClient2 = gdax.WebsocketClient(url="wss://ws-feed.gdax.com", products="ETH-USD",
+  mongo_collection=ETH_collection, should_print=True)
+wsClient3 = gdax.WebsocketClient(url="wss://ws-feed.gdax.com", products="LTC-USD",
+  mongo_collection=LTC_collection, should_print=True)
+wsClient1.start()
+wsClient2.start()
+wsClient3.start()
