ba097e333aa903b70b9a828d61bfe817ac815ce1
Matt Bernardini <matt.bernardini@gmail.com>
2017-12-12 21:24:43 -0600

    updated stuff

--- gdaxstreamer.py
+++ gdaxstreamer.py
@@ -6,33 +6,9 @@ mongo_client = MongoClient("mongodb://localhost:27017/")
 from websocket import WebSocketConnectionClosedException
 # specify the database and collection
 db = mongo_client.cryptocurrency_database
-BTC_collection = db.BTC_collection
-ETH_collection = db.ETH_collection
-LTC_collection = db.LTC_collection
+collection = db.unifiedCollection
 # instantiate a WebsocketClient instance, with a Mongo collection as a parameter
-wsClientBTC_USD = gdax.WebsocketClient(url="wss://ws-feed.gdax.com", products="BTC-USD",
-  mongo_collection=BTC_collection, should_print=False)
-wsClientETH_USD = gdax.WebsocketClient(url="wss://ws-feed.gdax.com", products="ETH-USD",
-  mongo_collection=ETH_collection, should_print=False)
-wsClientLTC_USD = gdax.WebsocketClient(url="wss://ws-feed.gdax.com", products="LTC-USD",
-  mongo_collection=LTC_collection, should_print=False)
-while (True):
-      try:
-        wsClientBTC_USD.start()
-        wsClientETH_USD.start()
-        wsClientLTC_USD.start()
-      except WebSocketConnectionClosedException as e:
-        print("\nCaught an exception")
-        if wsClientBTC_USD.stop:
-          print("\nwsClientBTC_USD broke")
-          wsClientBTC_USD.close()
-          wsClientBTC_USD.start()
-        elif wsClientETH_USD.stop:
-          print("\nwsClientETH_USD broke")
-          wsClientETH_USD.close()
-          wsClientETH_USD.start()
-        elif wsClientLTC_USD.stop:
-          print("\nwsClientLTC_USD broke")
-          wsClientLTC_USD.close()
-          wsClientLTC_USD.start()
+unifiedWsClient = gdax.WebsocketClient(url="wss://ws-feed.gdax.com", products=["BTC-USD","ETH-USD","LTC-USD",]
+  mongo_collection=collection, should_print=False)
+unifiedWsClient.start()
         
